6d5900480abdda00eca574ca2e572475a56a485e
juanescode <juanestebanvilladagallego23@gmail.com>
2024-07-12 15:27:44 -0500

    f

--- lib/isAdministrator.ts
+++ lib/isAdministrator.ts
@@ -1,10 +1,8 @@
 export const isAdministrator = (userId: string | null | undefined) => {
-  // Lista de IDs de administradores permitidos
   const administrators = [
-    process.env.NEXT_PUBLIC_ADMINISTRATOR_ID1, // Asegúrate de definir estas variables de entorno
+    process.env.NEXT_PUBLIC_ADMINISTRATOR_ID1, 
     process.env.NEXT_PUBLIC_ADMINISTRATOR_ID2,
   ];
 
-  // Comprueba si el userId está en la lista de administradores
   return administrators.includes(userId as string);
 };
